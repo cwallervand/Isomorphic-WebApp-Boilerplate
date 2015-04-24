38c4cc37554c94f47679e7c8c5f7aa33692b204a
Christian Wallervand <cwallervand@gmail.com>
2015-04-24 15:05:49 +0200

    Added client.js for client funcitonality

---
+++ src/js/client.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let React = require('react');
+let TodoApp = React.createFactory(require('./components/MyApp'));
+
+let renderComponent = function(){
+  let mountNode = document.getElementById('app-container');
+  React.render(TodoApp(), mountNode);
+};
+
+if (typeof window !== 'undefined') {
+	console.log("BOOTSTRAP APPLICATION ON CLIENT");
+    window.onload = () => {
+    	//When window is loaded, render the main component
+      renderComponent();
+    }
+}
